1530de688f00dfe81b5db69961ee9e126ceefffc
Matt Mulvahill (peregrine) <mjmulvahill@gmail.com>
2017-06-28 12:35:41 -0600

    initial code cleanup.

--- R/readdata.R
+++ R/readdata.R
@@ -1,60 +1,76 @@
-readdata <-
-function(directory, clinicalfile, quantificationfile, linkfile, cvmax=0.50, missing=1, linktxt){
+readdata <- function(directory,
+                     clinicalfile,
+                     quantificationfile,
+                     linkfile,
+                     cvmax   = 0.50,
+                     missing = 1,
+                     linktxt) {
 
-### Read in data
-quant<-read.table(paste(directory,quantificationfile,sep=""), header=TRUE, sep=",")
-link<-read.table(paste(directory,linkfile,sep=""), header=TRUE, sep=",")
-clinical<-read.table(paste(directory,clinicalfile,sep=""), header=TRUE, sep=",")
+  ### Read in data
+  quant    <- read.table(paste(directory, quantificationfile, sep = ""), 
+                         header = TRUE, sep = ", ")
+  link     <- read.table(paste(directory, linkfile, sep = ""), 
+                         header = TRUE, sep = ", ")
+  clinical <- read.table(paste(directory, clinicalfile, sep = ""), 
+                         header = TRUE, sep = ", ")
 
-compounds<-nrow(quant)
-subjects<-nrow(clinical)
+  compounds <- nrow(quant)
+  subjects <- nrow(clinical)
 
-metaf<-matrix(NA, ncol=compounds,nrow=subjects)
-temp3<-matrix(NA, ncol=3, nrow=subjects*compounds)
-temp4<-matrix(NA, ncol=2, nrow=subjects*compounds)
-k<-1
+  metaf <- matrix(NA, ncol = compounds,nrow = subjects)
+  temp3 <- matrix(NA, ncol = 3, nrow = subjects * compounds)
+  temp4 <- matrix(NA, ncol = 2, nrow = subjects * compounds)
+  k <- 1
 
-rownames(quant)<-paste(quant$mz,"_",quant$rt,sep="")
-metab<-subset(quant, select=-c(mz,rt))
+  rownames(quant) <- paste(quant$mz, quant$rt, sep = "_")
+  metab <- subset(quant, select = -c(mz, rt))
 
-for (i in 1:subjects){
-temp<-matrix(c(metab[,i*3-2],metab[,i*3-1],metab[,i*3]),ncol=3)
-temp2<-matrix(NA,ncol=4,nrow=compounds)
-for (j in 1:compounds){
-if (temp[j,1]==missing){temp[j,1]<-NA}
-if (temp[j,2]==missing){temp[j,2]<-NA}
-if (temp[j,3]==missing){temp[j,3]<-NA}
-temp2[j,1]<-mean(temp[j,],na.rm=TRUE)
-temp2[j,2]<-sd(temp[j,],na.rm=TRUE)
-temp2[j,3]<-temp2[j,2]/temp2[j,1]
-temp2[j,4]<-length(na.omit(temp[j,]))
-if (temp2[j,1]=="NaN"){temp2[j,1]<-0}
-### Compound must be present in at least two replicates
-### Compound must have CV less than CVMax
-if (temp2[j,4] < 2 || temp2[j,3] > cvmax){
-temp2[j,1]<-0
-if (temp2[j,4] == 3) {
-temp2[j,1]<-median(temp[j,],na.rm=TRUE)
-temp3[k,]<-temp[j,]
-temp4[k,1]<-rownames(metab)[j]
-temp4[k,2]<-colnames(metab)[i]
-k<-k+1
-}
-}
-}
-metaf[i,]<-t(temp2[,1])
-}
-med_comp<-temp3[1:eval((compounds*subjects)-summary(temp3[,1])[7]),]
-med_comp2<-temp4[1:eval((compounds*subjects)-summary(temp3[,1])[7]),]
-med_comp<-cbind(med_comp,med_comp2)
+  for (i in 1:subjects) {
+    temp  <- matrix(c(metab[, (i * 3 - 2)], metab[, (i * 3 - 1)], metab[, (i * 3)]), ncol = 3)
+    temp2 <- matrix(NA, ncol = 4, nrow = compounds)
 
-colnames(metaf)<-rownames(metab)
+    for (j in 1:compounds){
+      if (temp[j, 1] == missing) temp[j, 1] <- NA 
+      if (temp[j, 2] == missing) temp[j, 2] <- NA 
+      if (temp[j, 3] == missing) temp[j, 3] <- NA 
+
+      temp2[j, 1] <- mean(temp[j, ], na.rm = TRUE)
+      temp2[j, 2] <- sd(temp[j, ], na.rm = TRUE)
+      temp2[j, 3] <- temp2[j, 2]/temp2[j, 1]
+      temp2[j, 4] <- length(na.omit(temp[j, ]))
+
+      if (temp2[j, 1] == "NaN") temp2[j, 1] <- 0
+
+      ### Compound must be present in at least two replicates
+      ### Compound must have CV less than CVMax
+      if (temp2[j, 4] < 2 || temp2[j, 3] > cvmax) {
+        temp2[j, 1] <- 0
+        if (temp2[j, 4]  ==  3) {
+          temp2[j, 1] <- median(temp[j, ], na.rm = TRUE)
+          temp3[k, ] <- temp[j, ]
+          temp4[k, 1] <- rownames(metab)[j]
+          temp4[k, 2] <- colnames(metab)[i]
+          k <- k + 1
+        }
+      }
+    }
+    metaf[i, ] <- t(temp2[, 1])
+  }
+
+  med_comp  <- temp3[1:eval((compounds * subjects) - summary(temp3[, 1])[7]), ]
+  med_comp2 <- temp4[1:eval((compounds * subjects) - summary(temp3[, 1])[7]), ]
+  med_comp  <- cbind(med_comp,med_comp2)
+
+  colnames(metaf) <- rownames(metab)
 
 
-link2<-matrix(colnames(metab),ncol=1)
-colnames(link2)<-"LCMS_Run_ID"
-link3<-unique(merge(link2, link, by.x="LCMS_Run_ID", by.y=as.character(linktxt))[2])
-rownames(metaf)<-link3[,1]
+  link2 <- matrix(colnames(metab), ncol = 1)
+  colnames(link2) <- "LCMS_Run_ID"
+  link3 <- unique(merge(link2, link, by.x = "LCMS_Run_ID", 
+                        by.y = as.character(linktxt))[2])
+  rownames(metaf) <- link3[, 1]
 
-list(sum_data1=metaf, clinical=clinical, medians=med_comp)
+  list(sum_data1 = metaf,
+       clinical  = clinical,
+       medians   = med_comp)
 }
