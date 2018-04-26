675b32deb8213a31e160900f3e212c814b930207
Matt <mjmulvahill@gmail.com>
2018-04-26 17:08:26 -0600

    progress on ms_impute -- halfmin might be working, partila progress on bpca

--- R/impute.R
+++ R/impute.R
@@ -1,147 +1,127 @@
-# #' Imputes dataset
-# #'
-# #' and
-# #' performs data imputation.
-# #'
-# #' @param msprepped Placeholder
-# #' @param impute_function Placeholder
-# #' @return Placeholder
-# #' @details minval Filtered dataset with missing values replaced by 1/2 minimum
-# #' observed value for that compound.
-# #' @details bpca Filtered dataset with missing values imputed by a Bayesian PCA
-# #' from PCAMethods package.
-# #' @details withzero Filtered dataset with no imputation.
-# #' @details count List of all compounds and the percent present for each
-# #' compound.
-# #' @references 
-# #'   Oba, S.et al.(2003) A Bayesian missing value estimation for gene
-# #'   expression profile data. Bioinformatics, 19, 2088-2096
-# #'
-# #'   Stacklies, W.et al.(2007) pcaMethods A bioconductor package providing
-# #'   PCA methods for incomplete data. Bioinformatics, 23, 1164-1167.
-# #' @examples
-# #' library(magrittr)
-# #'
-# #' # Load object generated from readdata() function
-# #' data(msquant)
-# #'
-# #' filtered_data <- msquant %>% ms_tidy %>% ms_prepare %>% ms_filter(0.80)
-# #' imputed_data <- ms_impute(filtered_data, 0.80)
-# #'
-# #' @export
-# ms_impute <- function(msprepped, impute_function = ~ minval(.x)) {
-# 
-#   stopifnot("msprepped" %in% class(msprepped))
-# 
-#   # replace 0's with NA's for minval and pca() (all methods?)
-#   data <- msprepped$summary_data %>% 
-#     mutate_at(vars(abundance_summary), replace_missing, 0) 
-# 
-#   data %>% impute_function
-# 
-# }
 
+#' Imputes dataset
+#'
+#' Performs data imputation.
+#'
+#' @param msprepped Placeholder
+#' @param impute_function Placeholder
+#' @return Placeholder
+#' @details minval Filtered dataset with missing values replaced by 1/2 minimum
+#' observed value for that compound.
+#' @details bpca Filtered dataset with missing values imputed by a Bayesian PCA
+#' from PCAMethods package.
+#' @details withzero Filtered dataset with no imputation.
+#' @details count List of all compounds and the percent present for each
+#' compound.
+#' @references 
+#'   Oba, S.et al.(2003) A Bayesian missing value estimation for gene
+#'   expression profile data. Bioinformatics, 19, 2088-2096
+#'
+#'   Stacklies, W.et al.(2007) pcaMethods A bioconductor package providing
+#'   PCA methods for incomplete data. Bioinformatics, 23, 1164-1167.
+#' @examples
+#' library(magrittr)
+#'
+#' # Load object generated from readdata() function
+#' data(msquant)
+#'
+#' filtered_data <- msquant %>% ms_tidy %>% ms_prepare %>% ms_filter(0.80)
+#' imputed_data <- ms_impute(filtered_data, 0.80)
+#'
+#' @importFrom dplyr case_when
+#' @export
+ms_impute <- function(msprepped,
+                      method = c("halfmin", "bpca", "bpca + halfmin", "knn")) {
 
-
-ms_impute <- function(msprepped, impute_function = ~ minval(.x)) {
-
+  # Validate inputs
   stopifnot("msprepped" %in% class(msprepped))
+  match.arg(method) # requires 1 argument from vec in function arg
+  #check_ms_flow() # for checking valid path through pipeline
 
-  # replace 0's with NA's for minval and pca() (all methods?)
-  data <- msprepped$summary_data %>% 
-    mutate_at(vars(abundance_summary), replace_missing, 0)
-  # pca imputation
-  metabpca <- data_to_wide_matrix(data) %>% pcaMethods::pca(., nPcs = 3, method = "bpca")
-  # k-nearest-neighbors imputation
-  knnimpute <- data_to_wide_matrix(data) %>% as.data.frame %>% VIM::kNN(., k=5)
-  #[1:ncol(data), 1:nrow(data)]
+  # Prep data - replace 0's with NA's -- for minval and bpca() (all methods?)
+  data <- mutate_at(msprepped$summary_data, vars(abundance_summary), 
+                    replace_missing, 0)
 
-  data %>% impute_function
+  # Impute data
+  data <-
+    case_when(method == "halfmin"        ~ impute_halfmin(data),
+              method == "bpca"           ~ impute_bpca(data),
+              method == "bpca + halfmin" ~ impute_bpca_halfmin(data),
+              method == "knn"               ~ impute_knn(data),
+              TRUE ~ stop("Invalid impute method - you should never see this warning."))
 
-}
+  # Prep output object
+  msprepped$summary_data  <- data
+  msprepped$impute_method <- method
+  class(msprepped) <- c("msimputed", class(msprepped))
+  #msprepped <- mark_completed(msprepped, "imputation") # mark pipeline step as complete
 
-# Filterft to only include compounds that are found in specified percentage of
-# subjects and perform imputation of missing data
-
-# Manuscript description: 
-# Filtering: The resulting summarized dataset contains all compounds with one
-# observation per subject (or sample). The next processing step filters the data
-# to only compounds found in a user-specified percentage of subjects.
-
-data_to_wide_matrix <- function(data) {
-
-  data %>% 
-    unite(col = sample, subject_id, spike) %>% 
-    unite(col = compound, mz, rt) %>% 
-    spread(key = compound, value = abundance_summary)  %>%
-    as.data.frame %>%
-    column_to_rownames(var = "sample") %>%
-    as.matrix
+  # ...and:
+  return(msprepped)
 
 }
 
+# Old return
+#list(minval = as.data.frame(minval), withzero = as.data.frame(metafin), 
+#     bpca = as.data.frame(bpca), count = as.data.frame(count))
 
 
-#  colnames(toss) <- "toss"
-#  tests <- cbind(toss, t(metaf))
-#  metafin <- t(subset(tests, toss != 0)[, -1])
-#  metaimp <- matrix(NA, nrow = nrow(metafin), ncol = ncol(metafin))
-#  for (i in 1:nrow(metafin)) {
-#    for (j in 1:ncol(metafin)) {
-#      # sean jacobson added this so that missing data would be ignored
-#      # if(is.na(metafin[i, j])) next
-#      if (metafin[i, j] == 0) {
-#        metaimp[i, j] <- NA
-#      }
-#      else {
-#        metaimp[i, j] <- metafin[i, j]
-#      }
-#    }
-#  }
+
+#' @importFrom dplyr group_by
+#' @importFrom dplyr mutate
+#' @importFrom dplyr ungroup
+# Imputation using half of the minimum value 
+# should this be within subject or across all datapoints?  currently across
+# all
+impute_halfmin <- function(data) {
+
+  data <- group_by(data, spike, mz, rt)
+  data <- mutate(data, 
+                 abundance_summary = ifelse(is.na(abundance_summary),
+                                            min(abundance_summary, na.rm = TRUE)/2,
+                                            abundance_summary))
+  data <- ungroup(data)
+
+  return(data)
+
+}
+
+
+
+##' @importFrom pcaMethods pca
+##' @importFrom pcaMethods completeObs
+#impute_bpca <- function(data) {
 #
-#}
+#  # 1. Bayesian pca imputation
+#  data <- data_to_wide_matrix(data) 
+#  data <- pcaMethods::pca(., nPcs = 3, method = "bpca")
 #
-#  present <- matrix(1, nrow = nrow(metaf), ncol = ncol(metaf))
-#  colnames(present) <- colnames(metaf)
-#  rownames(present) <- rownames(metaf)
-#  for (j in 1:eval(ncol(metaf))) {
-#    for (i in 1:dim(metaf)[1]) {
-#      # sean jacobson added this for missing data
-#      # if(is.na(metaf[i, j])){present[i,j] <- NA; next}
-#      if (metaf[i, j] == 0) {
-#        present[i, j] <- 0
-#      }
-#    }
-#  }
-#
-#  minval <- metafin
-#  for (i in 1:nrow(metaf)) {
-#    for (j in 1:ncol(metafin)) {
-#      # sean jacobson added this for missing data
-#      # if(is.na(metafin[i, j])){minval[i,j] <- NA; next}
-#      if (metafin[i, j] == 0) {
-#        minval[i, j] <- summary(metaimp[, j])[1]/2
-#      }
-#      else {
-#        minval[i, j] <- metafin[i, j]
-#      }
-#    }
-#  }
-#
-#  metabpca <- pcaMethods::pca(metaimp, nPcs = 3, method = "bpca")
-#  bpca <- completeObs(metabpca)
-#  colnames(bpca) <- colnames(metafin)
+#  bpca <- completeObs(metabpca) # just extracts imputed dataset I think
+#  colnames(bpca) <- colnames(metafin) # add names back
 #  rownames(bpca) <- rownames(metafin)
-#  coldrop <- 1
-#  for (i in 1:nrow(bpca)) {
-#    for (j in 1:ncol(bpca)) {
-#      if (bpca[i, j] < 0) {
-#        bpca[i, j] <- minval[i, j]
-#      }
-#    }
-#  }
-#  list(minval = as.data.frame(minval), withzero = as.data.frame(metafin), 
-#    bpca = as.data.frame(bpca), count = as.data.frame(count))
+#  # if bpca < 0, then set to minval ??? WHAT -- combined bpca & minval imputation
+#  # check if this is right
+#
 #}
+#
+#
+#impute_bpca_halfmin <- function(data) {
+#}
+#
+#impute_knn <- function(data) {
+#  # 2. k-nearest-neighbors imputation
+#  knnimpute <- data_to_wide_matrix(data) %>% as.data.frame %>% VIM::kNN(., k=5)
+#}
+#
+#
+## OTHER METHODS
+## 4. zero imputation -- 0 becomes 0.0001 (why?)
+## 5. median imputation
+## 6. mean imputation
+#
+#
+#
+#
+#
 #
 #
