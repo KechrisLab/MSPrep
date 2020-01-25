57cd59d3ed7d9b7ae5621ad770da0bfa5aad932d
Max McGrath <max.a.mcgrath@gmail.com>
2020-01-24 19:52:50 -0700

    Add documentation for COPD_131

---
+++ R/COPD_131.R
@@ -0,0 +1,15 @@
+#' Example mass spectrometry dataset
+#' 
+#' Size: 662 obs. of 396 variables
+#' 
+#' Data contains samples for 131 subjects with 3 technical replicates per
+#' subject. Additionally, the first three variables are "Mass" which
+#' is the mass-to-charge ratio for each metabolite, "Retention.Time", and
+#' "Compound.Name".
+#' 
+#' @docType data
+#' 
+#' @keywords datasets
+#' @examples
+#' data(COPD_131)
+"COPD_131"
