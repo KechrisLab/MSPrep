96df1aa99051dd19ba21a4a98f2da9927e743988
Max McGrath <max.a.mcgrath@gmail.com>
2020-09-30 08:47:06 -0600

    Cleans up tests

--- tests/testthat/test_sedf_equality.R
+++ tests/testthat/test_sedf_equality.R
@@ -4,30 +4,17 @@ set.seed(9997)
 
 data(msquant)
 
-## Create SE from MSQuant
-colnames <- colnames(msquant)[3:56] %>% 
-    str_replace_all("Neutral_Operator_Dif_Pos_", "")
+se <- .dfToSE(msquant,
+              compVars = c("mz", "rt"),
+              sampleVars = c("spike", "batch", "replicate", "subject_id"),
+              colExtraText = "Neutral_Operator_Dif_Pos_",
+              separator = "_")
 
-spike <- vector("character", 54)
-replicate <- vector("character", 54)
-batch <- vector("character", 54)
-subject_id <- vector("character", 54)
-
-for(i in 1:54) {
-    spike[i] <- stringr::str_split(colnames, "_")[[i]][1]
-    batch[i] <- stringr::str_split(colnames, "_")[[i]][2]
-    replicate[i] <- stringr::str_split(colnames, "_")[[i]][3]
-    subject_id[i] <- stringr::str_split(colnames, "_")[[i]][4]
-}
-
-colnamesDF <- data.frame(spike, batch, replicate, subject_id)
-rownamesDF <- msquant[1:2]
-abundanceMatrix <- as.matrix(msquant[3:56])
-rownames(abundanceMatrix) <- c()
-
-se <- SummarizedExperiment(abundanceMatrix, colData = colnamesDF, 
-                           rowData = rownamesDF, 
-                           metadata = "Test Metadata")
+metadata(se) <- list(test1 = "test",
+                     test2 = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3),
+                     test3 = data.frame(test1 = c(1, 2, 3),
+                                        test2 = c(4, 5, 6)),
+                     test4 = TRUE)
 
 ## Run msTidy with SE and data frame
 tidySE <- .msTidy(se, missingValue = 1)
